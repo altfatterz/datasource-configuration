8b392647c6e844f216ae352c8e299918e877cb21
Zoltan Altfatter <altftterz@gmail.com>
2014-10-19 14:43:42 +0200

    add changes to be able to run in a Servlet 3.0 container

--- src/main/java/com/backbase/progfun/Application.java
+++ src/main/java/com/backbase/progfun/Application.java
@@ -5,18 +5,33 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 import javax.annotation.PostConstruct;
 
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    /*
+       Used for traditional deployment into a Servlet 3.0 container.
+       SpringBootServletInitializer is a WebApplicationInitializer which is detected automatically by
+       SpringServletContainerInitializer which itself is bootstrapped automatically by any Servlet 3.0 container
+
+       Servlet 3.0 ServletContainerInitializer is designed to support code-based configuration as opposed to
+       the traditional web.xml based approach.
+    */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 
     @Autowired(required = false)
